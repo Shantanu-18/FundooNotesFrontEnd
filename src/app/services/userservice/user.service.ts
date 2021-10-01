2e9dcf1ecc6c670f35657d64d360b3210642a9b1
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-01 12:35:54 +0530

    Added: registerSercive method in User Services

--- src/app/services/userservice/user.service.ts
+++ src/app/services/userservice/user.service.ts
@@ -1,9 +1,24 @@
+import { HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpService } from '../httpService/http.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  token: any
+  BaseUrl = environment.BaseUrl
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
+
+  registerSercive(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json',
+        Authorization: this.token
+      })
+    }
+    return this.httpService.PostService(this.BaseUrl + '/User/Registration', reqData, false,headers);
+  }
 }
