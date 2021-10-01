f9a53ce5404be71fe85a184858411a0e58779957
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-01 12:35:01 +0530

    Added: Http services like post, Get, Put, Delete

--- src/app/services/httpservice/http.service.ts
+++ src/app/services/httpservice/http.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  PostService(url = '', payload = null, token: boolean = false, headers:any = null) {
+    return this.http.post(url, payload, token && headers)
+  }
+
+  GetService() {
+
+  }
+
+  PutService() {
+
+  }
+
+  DeleteService() {
+
+  }
 }
