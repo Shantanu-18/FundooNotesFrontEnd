691dea4caddef3f81e9927ed16eb6f94e805d352
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-03 11:46:46 +0530

    Added: loginService, forgotPasswordService, resetPasswordService

--- src/app/services/userservice/user.service.ts
+++ src/app/services/userservice/user.service.ts
@@ -19,6 +19,36 @@ export class UserService {
         Authorization: this.token
       })
     }
-    return this.httpService.PostService(this.BaseUrl + '/User/Registration', reqData, false,headers);
+    return this.httpService.PostService(this.BaseUrl + '/User/Registration', reqData, false, headers);
+  }
+
+  loginService(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json',
+        Authorization: this.token
+      })
+    }
+    return this.httpService.PostService(this.BaseUrl + '/User/Login', reqData, true, headers);
+  }
+
+  forgotPasswordService(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json',
+        Authorization: this.token
+      })
+    }
+    return this.httpService.PostService(this.BaseUrl + '/User/ForgotPassword', reqData, false, headers);
+  }
+
+  resetPasswordService(reqData: any) {
+    let headers = {
+      headers: new HttpHeaders({
+        'content-Type': 'application/json',
+        Authorization: this.token
+      })
+    }
+    return this.httpService.PutService(this.BaseUrl + '/User/ResetPassword', reqData, true, headers);
   }
 }
