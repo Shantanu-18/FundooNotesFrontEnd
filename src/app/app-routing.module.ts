9f536a009cc010e11c640ef038309a733de836b7
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-06 10:18:35 +0530

    Added: Dashboard path and children path for dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,20 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
-import { NotesComponent } from './components/notes/notes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgotPassword', component: ForgotPasswordComponent },
   { path: 'resetPassword/:token', component: ResetPasswordComponent },
-  { path: 'notes', component: NotesComponent }
+  { path: 'dashboard', component: DashboardComponent ,
+  children:[
+  { path: 'notes', component: GetAllNotesComponent },
+
+  ]
+}
 ];
 
 @NgModule({
