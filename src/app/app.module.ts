a695ad27ea733a141d5b92adbd4c486abd984591
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-06 09:57:21 +0530

    Added: Dashboard, GetAllNotes, Icons, CreateNotes, DisplayNotes Components and MatExpansionModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,11 +16,16 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { NotesComponent } from './components/notes/notes.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
+import { IconsComponent } from './components/icons/icons.component';
+import { CreateNotesComponent } from './components/create-notes/create-notes.component';
+import { DisplayNotesComponent } from './components/display-notes/display-notes.component';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -29,7 +34,11 @@ import { MatListModule } from '@angular/material/list';
     RegisterComponent,
     ForgotPasswordComponent,
     ResetPasswordComponent,
-    NotesComponent
+    DashboardComponent,
+    GetAllNotesComponent,
+    IconsComponent,
+    CreateNotesComponent,
+    DisplayNotesComponent
   ],
   imports: [
     BrowserModule,
@@ -48,7 +57,8 @@ import { MatListModule } from '@angular/material/list';
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
