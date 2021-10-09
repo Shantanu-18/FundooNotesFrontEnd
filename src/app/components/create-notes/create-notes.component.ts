d19406c71c9dcf48ef12c6b20a365eedd5ed9c7b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-09 20:25:32 +0530

    Added: code to send data to services

--- src/app/components/create-notes/create-notes.component.ts
+++ src/app/components/create-notes/create-notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NotesService } from 'src/app/services/noteService/notes.service';
 
 @Component({
   selector: 'app-create-notes',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-notes.component.scss']
 })
 export class CreateNotesComponent implements OnInit {
+  panelOpenState = false;
+  createNotesForm!: FormGroup;
   
-  constructor() { }
-
+  constructor(private notesService: NotesService, private formBuilder: FormBuilder) { }
+  
+  @Output() noteCreated= new EventEmitter<any>();
+  
   ngOnInit(): void {
+    this.createNotesForm = this.formBuilder.group({
+      title: [],
+      takeNote: []
+    })
   }
 
+  onClose() {
+    if (this.createNotesForm.value.title != null && this.createNotesForm.value.takeNote != null) {
+      console.log(this.createNotesForm.value);
+
+      let reqPayload = {
+        title: this.createNotesForm.value.title,
+        message: this.createNotesForm.value.takeNote
+      }
+
+      this.notesService.createNoteService(reqPayload).subscribe((response:any)=>{
+        console.log(response);
+        this.noteCreated.emit(response);
+      })
+    }
+  }
 }
