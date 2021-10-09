19c79645ba3c1ba5300f8518145b214ab652bc3b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-09 20:41:26 +0530

    Added: Authguard to stop unauthenticated user to access specific urls

---
+++ src/app/services/authentication.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthguardServiceService } from './AuthguardService/authguard-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationGuard implements CanActivate {
+
+  constructor(private authguardService: AuthguardServiceService, private router: Router) {} 
+
+  canActivate(): boolean {
+    if(!this.authguardService.getToken()){
+      this.router.navigateByUrl("/login");
+    }
+    return this.authguardService.getToken();
+  }
+}
