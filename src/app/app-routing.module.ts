a26198a0ca56cbb988224def74a8bfc0d7096a41
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-09 20:03:24 +0530

    Added: AuthenticationGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,21 @@ import { ForgotPasswordComponent } from './components/forgot-password/forgot-pas
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { GetAllNotesComponent } from './components/get-all-notes/get-all-notes.component';
+import { AuthenticationGuard } from './services/authentication.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgotPassword', component: ForgotPasswordComponent },
   { path: 'resetPassword/:token', component: ResetPasswordComponent },
-  { path: 'dashboard', component: DashboardComponent ,
-  children:[
-  { path: 'notes', component: GetAllNotesComponent },
+  {
+    path: 'dashboard', component: DashboardComponent, canActivate: [AuthenticationGuard],
+    children: [
+      { path: 'notes', component: GetAllNotesComponent },
 
-  ]
-}
+    ]
+  }
 ];
 
 @NgModule({
