6fcc3ba65c22a1890a4eb1e49efed652d36d2fcf
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-09 20:30:14 +0530

    Added: code to open dialog box and send data to update component

--- src/app/components/display-notes/display-notes.component.ts
+++ src/app/components/display-notes/display-notes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateComponent } from '../update/update.component';
 
 @Component({
   selector: 'app-display-notes',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-notes.component.scss']
 })
 export class DisplayNotesComponent implements OnInit {
+  @Input() notesArray: any;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+  
+  openDialog(note:any) {
+    const dialogRef = this.dialog.open(UpdateComponent,{
+      panelClass: 'dialog-container-custom',
+      width:"600px",
+      height:"auto",
+      data:note
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
