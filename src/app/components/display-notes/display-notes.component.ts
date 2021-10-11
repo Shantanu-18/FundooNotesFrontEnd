e3a1db5c603b423c523dabba0ee0d4e2b2a4ce38
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-11 23:16:52 +0530

    Added: event emitter for data binding

--- src/app/components/display-notes/display-notes.component.ts
+++ src/app/components/display-notes/display-notes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UpdateComponent } from '../update/update.component';
 
@@ -11,20 +11,25 @@ export class DisplayNotesComponent implements OnInit {
   @Input() notesArray: any;
 
   constructor(public dialog: MatDialog) { }
+  @Output() noteUpdated = new EventEmitter<any>();
 
   ngOnInit(): void {
   }
-  
-  openDialog(note:any) {
-    const dialogRef = this.dialog.open(UpdateComponent,{
+
+  openDialog(note: any) {
+    const dialogRef = this.dialog.open(UpdateComponent, {
       panelClass: 'dialog-container-custom',
-      width:"600px",
-      height:"auto",
-      data:note
+      width: "600px",
+      data: note
     });
 
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
+      this.noteUpdated.emit(result);
     });
+  }
+
+  operation(value: any) {
+    this.noteUpdated.emit(value);
   }
 }
