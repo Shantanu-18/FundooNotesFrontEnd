b184dae2dfdc5bb38e45932e01b656b0b49e095e
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-10-11 23:18:03 +0530

    Added: Error handling and printing error on snack bar

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/userService/user.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
@@ -13,7 +14,7 @@ export class ForgotPasswordComponent implements OnInit {
   forgotPassword!: FormGroup;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private userService: UserService, private snackbar: MatSnackBar) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private snackbar: MatSnackBar, private router: Router) { }
 
   ngOnInit(): void {
     this.forgotPassword = this.formBuilder.group({
@@ -43,7 +44,10 @@ export class ForgotPasswordComponent implements OnInit {
 
       this.userService.forgotPasswordService(reqPayload).subscribe((res) => {
         console.log(res)
-      })
+      }, error => {
+        this.snackbar.open(error, '', { duration: 1500 });
+        console.log(error);
+      });
     }
   }
 }
